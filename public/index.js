4effa1dfb63e44718dc85f83d6730dc64bfd0df9
WhalenSITHS <michael.whalen@sitechhs.com>
2019-12-18 11:45:28 -0500

    day 2

--- public/index.js
+++ public/index.js
@@ -1,15 +1,56 @@
 const path = require("path");
+const hbs = require("hbs");
 const express = require("express");
 const app = express();
 
-app.get("", (req, res) => {
-  res.send("Thami likes bagels and butter. Also he smells");
+//define paths for express
+const publicDirectoryPath = path.join(__dirname, "../public");
+const partialsPath = path.join(__dirname, "../templates/partials");
+const viewsPath = path.join(__dirname, "../templates/views");
+//setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath); //telling express to get templates from templates/views folder
+hbs.registerPartials(partialsPath);
+app.get("", async (req, res) => {
+  try {
+    res.render("index", {
+      title: "Our First Express App"
+    });
+  } catch {
+    res.status(500).send();
+  }
 });
-app.get("/thamisucks", (req, res) => {
-  res.send("Thami sucks. And Yes I have to do this while you eat");
+app.get("/about/:id", async (req, res) => {
+  const name = req.params.id;
+  try {
+    res.render("index", {
+      title: name
+    });
+  } catch {
+    res.status(500).send();
+  }
 });
+app.get("/anything", async (req, res) => {
+  try {
+    res.render("swag");
+  } catch {
+    res.status(500).send();
+  }
+});
+/* app.get("/about/:id", async (req, res) => {
+  const test = req.params.id;
+  try {
+    res.render("index", {
+      title: `${test}`
+    });
+  } catch {
+    res.status(500).send();
+  }
+}); */
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
+  console.log(viewsPath);
 });
 
 //127.0.0.1:3000
